e9ba8450c0d6645143b715e2215cc496b7bb9978
Arjun Junna <arjun9852@gmail.com>
2024-05-06 13:56:40 +0530

    feat : add user checks to create new user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,23 +11,39 @@ export class UsersService {
     private prisma: PrismaService,
     private jwtService: JwtService,
   ) {}
-
+  
   async createUser(data: Prisma.UserCreateInput) {
-    const hashedPassword = await bcrypt.hash(data.password, roundsOfHashing);
+    try {
+      const existingUser = await this.prisma.user.findFirst({
+        where: {
+          OR: [{ username: data.username }, { email: data.email }],
+        },
+      });
 
-    data.password = hashedPassword;
-    const newUser = await this.prisma.user.create({
-      data,
-    });
+      if (existingUser) {
+        let field;
+        if (existingUser.username === data.username) {
+          field = 'username';
+        } else {
+          field = 'email';
+        }
+        return { status: 409, message: `The ${field} is already taken.` };
+      }
+      const hashedPassword = await bcrypt.hash(data.password, roundsOfHashing);
+      data.password = hashedPassword;
+      const newUser = await this.prisma.user.create({ data });
 
-    const userDetails = await this.prisma.user.findUnique({
-      where: {
-        username: newUser.username,
-      },
-    });
-    const { password, ...user } = userDetails;
-    const token= this.jwtService.sign(user);
-    return {...user,token}
+      const userDetails = await this.prisma.user.findUnique({
+        where: {
+          username: newUser.username,
+        },
+      });
+      const { password, ...user } = userDetails;
+      const token = this.jwtService.sign(user);
+      return { ...user, token };
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   getUsers() {
