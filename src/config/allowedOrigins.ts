93c6fd25a96c6e954ec9aa97638b903d5ff2c406
Arjun Junna <arjun9852@gmail.com>
2024-06-03 22:09:16 +0530

    refactor allowed origins

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,1 +1,5 @@
-export const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
+export const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'https://zodiac-hub.vercel.app/',
+];
